471070deb37dfc439628f7f53252b271d6ead8ad
beevageeva <beevageeva@users.noreply.github.com>
2024-02-05 20:40:42 +0000

    dev: deploy amrvac/amrvac@9123b07b46beae489321e937ce1da529ea503fed

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['rc_5ffluid_3401',['rc_fluid',['../structmod__radiative__cooling_1_1rc__fluid.html',1,'mod_radiative_cooling']]],
-  ['refine_5fgrid_3402',['refine_grid',['../interfacemod__usr__methods_1_1refine__grid.html',1,'mod_usr_methods']]],
-  ['rfactor_3403',['Rfactor',['../interfacemod__usr__methods_1_1Rfactor.html',1,'mod_usr_methods']]]
+  ['rc_5ffluid_3402',['rc_fluid',['../structmod__radiative__cooling_1_1rc__fluid.html',1,'mod_radiative_cooling']]],
+  ['refine_5fgrid_3403',['refine_grid',['../interfacemod__usr__methods_1_1refine__grid.html',1,'mod_usr_methods']]],
+  ['rfactor_3404',['Rfactor',['../interfacemod__usr__methods_1_1Rfactor.html',1,'mod_usr_methods']]]
 ];
