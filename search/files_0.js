1ab5ec2302fba806ee1f0b94b69d18a465a08cb8
99xc41 <99xc41@users.noreply.github.com>
2024-04-22 08:58:50 +0000

    dev: deploy amrvac/amrvac@ea14a65b5e1366bfd3b890d9f9d210f431c0a597

--- search/files_0.js
+++ search/files_0.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['acknowledgments_2emd_3621',['acknowledgments.md',['../acknowledgments_8md.html',1,'']]],
-  ['addmodule_2emd_3622',['addmodule.md',['../addmodule_8md.html',1,'']]],
-  ['advection_5freaction_5fdiffusion_2emd_3623',['advection_reaction_diffusion.md',['../advection__reaction__diffusion_8md.html',1,'']]],
-  ['alfvenwing_2emd_3624',['alfvenwing.md',['../alfvenwing_8md.html',1,'']]],
-  ['amrstructure_2emd_3625',['amrstructure.md',['../amrstructure_8md.html',1,'']]],
-  ['amrvac_2et_3626',['amrvac.t',['../amrvac_8t.html',1,'']]],
-  ['amrvacusr_2emd_3627',['amrvacusr.md',['../amrvacusr_8md.html',1,'']]],
-  ['analysis_2emd_3628',['analysis.md',['../analysis_8md.html',1,'']]],
-  ['axial_2emd_3629',['axial.md',['../axial_8md.html',1,'']]]
+  ['acknowledgments_2emd_3622',['acknowledgments.md',['../acknowledgments_8md.html',1,'']]],
+  ['addmodule_2emd_3623',['addmodule.md',['../addmodule_8md.html',1,'']]],
+  ['advection_5freaction_5fdiffusion_2emd_3624',['advection_reaction_diffusion.md',['../advection__reaction__diffusion_8md.html',1,'']]],
+  ['alfvenwing_2emd_3625',['alfvenwing.md',['../alfvenwing_8md.html',1,'']]],
+  ['amrstructure_2emd_3626',['amrstructure.md',['../amrstructure_8md.html',1,'']]],
+  ['amrvac_2et_3627',['amrvac.t',['../amrvac_8t.html',1,'']]],
+  ['amrvacusr_2emd_3628',['amrvacusr.md',['../amrvacusr_8md.html',1,'']]],
+  ['analysis_2emd_3629',['analysis.md',['../analysis_8md.html',1,'']]],
+  ['axial_2emd_3630',['axial.md',['../axial_8md.html',1,'']]]
 ];
--- search/pages_d.js
+++ search/pages_d.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['setting_20parameters_6799',['Setting parameters',['../md_doc_par.html',1,'']]],
-  ['setting_20up_20a_20new_20problem_6800',['Setting up a new problem',['../md_doc_amrvacusr.html',1,'']]],
-  ['setting_20up_20the_20python_20tools_6801',['Setting up the python tools',['../md_doc_python_setup.html',1,'']]],
-  ['slice_20output_6802',['Slice output',['../md_doc_slices.html',1,'']]],
-  ['slope_20limiters_6803',['Slope Limiters',['../md_doc_limiter.html',1,'']]],
-  ['small_20values_20options_20in_20mpi_2damrvac_6804',['Small values options in MPI-AMRVAC',['../md_doc_smallvalues.html',1,'']]],
-  ['source_20language_6805',['Source language',['../md_doc_source.html',1,'']]]
+  ['setting_20parameters_6801',['Setting parameters',['../md_doc_par.html',1,'']]],
+  ['setting_20up_20a_20new_20problem_6802',['Setting up a new problem',['../md_doc_amrvacusr.html',1,'']]],
+  ['setting_20up_20the_20python_20tools_6803',['Setting up the python tools',['../md_doc_python_setup.html',1,'']]],
+  ['slice_20output_6804',['Slice output',['../md_doc_slices.html',1,'']]],
+  ['slope_20limiters_6805',['Slope Limiters',['../md_doc_limiter.html',1,'']]],
+  ['small_20values_20options_20in_20mpi_2damrvac_6806',['Small values options in MPI-AMRVAC',['../md_doc_smallvalues.html',1,'']]],
+  ['source_20language_6807',['Source language',['../md_doc_source.html',1,'']]]
 ];
