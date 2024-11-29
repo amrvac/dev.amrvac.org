123d2f01292195a0374ab07c4e0e336317c4dfd4
fabsilfab <fabsilfab@users.noreply.github.com>
2024-11-29 10:13:38 +0000

    dev: deploy amrvac/amrvac@d34f2c5ac64ea6fccea435a73a111f3e6e03f635

--- search/pages_7.js
+++ search/pages_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['implosion_20test_20problem_3a_202d_20hd_20liska_2dwendroff_6784',['Implosion test problem: 2D HD Liska-Wendroff',['../md_tests_hd_implosion_2D_implosion.html',1,'']]],
-  ['installation_6785',['Installation',['../md_doc_installation.html',1,'']]]
+  ['icarus_6974',['icarus',['../md_tests_mhd_icarus_icarus.html',1,'']]],
+  ['implosion_20test_20problem_3a_202d_20hd_20liska_2dwendroff_6975',['Implosion test problem: 2D HD Liska-Wendroff',['../md_tests_hd_implosion_2D_implosion.html',1,'']]],
+  ['installation_6976',['Installation',['../md_doc_installation.html',1,'']]]
 ];
