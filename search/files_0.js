c8fb52b0d86048ca628a05278c6fdf4374dca70c
99xc41 <99xc41@users.noreply.github.com>
2025-01-22 09:28:01 +0000

    dev: deploy amrvac/amrvac@99c6dcd8b1b910fb5bf803a3a48322c0905ab830

--- search/files_0.js
+++ search/files_0.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['acknowledgments_2emd_3719',['acknowledgments.md',['../acknowledgments_8md.html',1,'']]],
-  ['addmodule_2emd_3720',['addmodule.md',['../addmodule_8md.html',1,'']]],
-  ['advection_5freaction_5fdiffusion_2emd_3721',['advection_reaction_diffusion.md',['../advection__reaction__diffusion_8md.html',1,'']]],
-  ['alfvenwing_2emd_3722',['alfvenwing.md',['../alfvenwing_8md.html',1,'']]],
-  ['amrstructure_2emd_3723',['amrstructure.md',['../amrstructure_8md.html',1,'']]],
-  ['amrvac_2et_3724',['amrvac.t',['../amrvac_8t.html',1,'']]],
-  ['amrvacusr_2emd_3725',['amrvacusr.md',['../amrvacusr_8md.html',1,'']]],
-  ['analysis_2emd_3726',['analysis.md',['../analysis_8md.html',1,'']]],
-  ['axial_2emd_3727',['axial.md',['../axial_8md.html',1,'']]]
+  ['acknowledgments_2emd_0',['acknowledgments.md',['../acknowledgments_8md.html',1,'']]],
+  ['addmodule_2emd_1',['addmodule.md',['../addmodule_8md.html',1,'']]],
+  ['advection_5freaction_5fdiffusion_2emd_2',['advection_reaction_diffusion.md',['../advection__reaction__diffusion_8md.html',1,'']]],
+  ['alfvenwing_2emd_3',['alfvenwing.md',['../alfvenwing_8md.html',1,'']]],
+  ['amrstructure_2emd_4',['amrstructure.md',['../amrstructure_8md.html',1,'']]],
+  ['amrvac_2et_5',['amrvac.t',['../amrvac_8t.html',1,'']]],
+  ['amrvacusr_2emd_6',['amrvacusr.md',['../amrvacusr_8md.html',1,'']]],
+  ['analysis_2emd_7',['analysis.md',['../analysis_8md.html',1,'']]],
+  ['axial_2emd_8',['axial.md',['../axial_8md.html',1,'']]]
 ];
--- search/files_4.js
+++ search/files_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['faq_2emd_3743',['faq.md',['../faq_8md.html',1,'']]],
-  ['features_2emd_3744',['features.md',['../features_8md.html',1,'']]],
-  ['fileformat_2emd_3745',['fileformat.md',['../fileformat_8md.html',1,'']]]
+  ['faq_2emd_0',['faq.md',['../faq_8md.html',1,'']]],
+  ['features_2emd_1',['features.md',['../features_8md.html',1,'']]],
+  ['fileformat_2emd_2',['fileformat.md',['../fileformat_8md.html',1,'']]]
 ];
