bbec1fbf82ad75d8a15771582b0050c05a54dd44
Astery0502 <Astery0502@users.noreply.github.com>
2025-07-03 07:19:53 +0000

    dev: deploy amrvac/amrvac@3ae5f2800fb547071ae36deb4b3ba6a267692af9

--- search/searchdata.js
+++ search/searchdata.js
@@ -4,7 +4,7 @@ var indexSectionsWithContent =
   1: "acfghilmprstuvw",
   2: "m",
   3: "acdefgilmprstvwy",
-  4: "abcdefghijklmnopqrstuvw",
+  4: "abcdefghijklmnoprstuvw",
   5: "abcdefghijklmnopqrstuvwxyz",
   6: "023acdefghilmnopqrstuvwy"
 };
