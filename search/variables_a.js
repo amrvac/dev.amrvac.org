3e106ee235ed1f75b39cddcf8571879a24ec0202
ronykeppens <ronykeppens@users.noreply.github.com>
2026-04-01 11:36:01 +0000

    dev: deploy amrvac/amrvac@bfc355526644fcb9031f2f8c8d7750be074ac351

--- search/variables_a.js
+++ search/variables_a.js
@@ -4,11 +4,10 @@ var searchData=
   ['kappae_5fvals_1',['kappae_vals',['../namespacemod__cak__opacity.html#adbf22c7880102b71259e10a7bbda2479',1,'mod_cak_opacity']]],
   ['kb_5fcgs_2',['kb_cgs',['../namespacemod__constants.html#a33660422af2eb7bceb7c9162c04c0688',1,'mod_constants']]],
   ['kb_5fsi_3',['kb_si',['../namespacemod__constants.html#a9cf643c04abeb836326c027924b53d7c',1,'mod_constants']]],
-  ['kbmpmua4_4',['kbmpmua4',['../namespacemod__rmhd__phys.html#a4b3a09d68f4d44b8c8c0e1367e4b3ee0',1,'mod_rmhd_phys']]],
-  ['kdv_5fdelta_5',['kdv_delta',['../namespacemod__kdv.html#abc5190eca5496e7f28668f0d9d92251f',1,'mod_kdv']]],
-  ['kdv_5forder_6',['kdv_order',['../namespacemod__kdv.html#a6d24bd8450000c731e6505d07e5046de',1,'mod_kdv']]],
-  ['kdv_5fsource_5fterm_7',['kdv_source_term',['../namespacemod__nonlinear__phys.html#a673d1030c11f7d04b8102184cde63dd6',1,'mod_nonlinear_phys']]],
-  ['kdv_5fsplit_8',['kdv_split',['../namespacemod__kdv.html#ad8ce6e5248e6db5c86d1e4e8b1635f4e',1,'mod_kdv']]],
-  ['kmaxx_9',['kmaxx',['../namespacemod__odeint.html#a4dd539fe2bf3bc713ff7b1275efecbd5',1,'mod_odeint']]],
-  ['kr_10',['kr',['../namespacemod__global__parameters.html#a31226ca76b94f6c33496105fd25e8fd9',1,'mod_global_parameters']]]
+  ['kdv_5fdelta_4',['kdv_delta',['../namespacemod__kdv.html#abc5190eca5496e7f28668f0d9d92251f',1,'mod_kdv']]],
+  ['kdv_5forder_5',['kdv_order',['../namespacemod__kdv.html#a6d24bd8450000c731e6505d07e5046de',1,'mod_kdv']]],
+  ['kdv_5fsource_5fterm_6',['kdv_source_term',['../namespacemod__nonlinear__phys.html#a673d1030c11f7d04b8102184cde63dd6',1,'mod_nonlinear_phys']]],
+  ['kdv_5fsplit_7',['kdv_split',['../namespacemod__kdv.html#ad8ce6e5248e6db5c86d1e4e8b1635f4e',1,'mod_kdv']]],
+  ['kmaxx_8',['kmaxx',['../namespacemod__odeint.html#a4dd539fe2bf3bc713ff7b1275efecbd5',1,'mod_odeint']]],
+  ['kr_9',['kr',['../namespacemod__global__parameters.html#a31226ca76b94f6c33496105fd25e8fd9',1,'mod_global_parameters']]]
 ];
