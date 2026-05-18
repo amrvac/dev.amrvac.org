f30bfe5c7a938ea3add8a07f4b4fbec5ec6b2904
nicolasbrughmans <nicolasbrughmans@users.noreply.github.com>
2026-05-18 14:31:32 +0000

    dev: deploy amrvac/amrvac@487b5566c668e6d4d0427c9605039dac12ddefcc

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['elle_0',['elle',['../namespacemod__tdfluxrope.html#a750dc22b6a0aa17fed081058f91bf3c9',1,'mod_tdfluxrope']]],
-  ['ellf_1',['ellf',['../namespacemod__tdfluxrope.html#a9748be368eea6a329fd2cf155044c6ff',1,'mod_tdfluxrope']]],
-  ['ellpi_2',['ellpi',['../namespacemod__tdfluxrope.html#adbf5a28df3c18ebc14bf87e2036a8c74',1,'mod_tdfluxrope']]],
-  ['end_5fcomm_5ffaces_3',['end_comm_faces',['../namespacemod__amr__fct.html#adbbdbcbea30b85f3f207ce8e03bc04d4',1,'mod_amr_fct']]],
-  ['entropyfix_4',['entropyfix',['../namespacemod__tvd.html#ae8748c5ff719141fefc2ac786f5b4acd',1,'mod_tvd']]],
-  ['errest_5',['errest',['../namespacemod__errest.html#aec4df070250188e621b93a27e0507ece',1,'mod_errest']]],
-  ['errormsg_6',['errormsg',['../namespacemod__variables.html#a67d7a43716758f18d4a148274819a39a',1,'mod_variables']]],
-  ['evaluate_5fdiffterm_5fonegrid_7',['evaluate_diffterm_onegrid',['../namespacemod__fld.html#a5bc7da4fdd7a34dfc265c91589f0efab',1,'mod_fld']]]
+  ['ef_5famplitude_0',['ef_amplitude',['../namespacemod__gimli.html#a90f975c33477e04c34005a0f018f5500',1,'mod_gimli']]],
+  ['elle_1',['elle',['../namespacemod__tdfluxrope.html#a750dc22b6a0aa17fed081058f91bf3c9',1,'mod_tdfluxrope']]],
+  ['ellf_2',['ellf',['../namespacemod__tdfluxrope.html#a9748be368eea6a329fd2cf155044c6ff',1,'mod_tdfluxrope']]],
+  ['ellpi_3',['ellpi',['../namespacemod__tdfluxrope.html#adbf5a28df3c18ebc14bf87e2036a8c74',1,'mod_tdfluxrope']]],
+  ['end_5fcomm_5ffaces_4',['end_comm_faces',['../namespacemod__amr__fct.html#adbbdbcbea30b85f3f207ce8e03bc04d4',1,'mod_amr_fct']]],
+  ['entropyfix_5',['entropyfix',['../namespacemod__tvd.html#ae8748c5ff719141fefc2ac786f5b4acd',1,'mod_tvd']]],
+  ['errest_6',['errest',['../namespacemod__errest.html#aec4df070250188e621b93a27e0507ece',1,'mod_errest']]],
+  ['errormsg_7',['errormsg',['../namespacemod__variables.html#a67d7a43716758f18d4a148274819a39a',1,'mod_variables']]],
+  ['evaluate_5fdiffterm_5fonegrid_8',['evaluate_diffterm_onegrid',['../namespacemod__fld.html#a5bc7da4fdd7a34dfc265c91589f0efab',1,'mod_fld']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['kappa_5fvals_0',['kappa_vals',['../namespacemod__opal__opacity.html#a7dd97579ace2715cddebb4296ab3101f',1,'mod_opal_opacity']]],
-  ['kappae_5fvals_1',['kappae_vals',['../namespacemod__cak__opacity.html#adbf22c7880102b71259e10a7bbda2479',1,'mod_cak_opacity']]],
-  ['kb_5fcgs_2',['kb_cgs',['../namespacemod__constants.html#a33660422af2eb7bceb7c9162c04c0688',1,'mod_constants']]],
-  ['kb_5fsi_3',['kb_si',['../namespacemod__constants.html#a9cf643c04abeb836326c027924b53d7c',1,'mod_constants']]],
-  ['kdv_5fdelta_4',['kdv_delta',['../namespacemod__kdv.html#abc5190eca5496e7f28668f0d9d92251f',1,'mod_kdv']]],
-  ['kdv_5forder_5',['kdv_order',['../namespacemod__kdv.html#a6d24bd8450000c731e6505d07e5046de',1,'mod_kdv']]],
-  ['kdv_5fsource_5fterm_6',['kdv_source_term',['../namespacemod__nonlinear__phys.html#a673d1030c11f7d04b8102184cde63dd6',1,'mod_nonlinear_phys']]],
-  ['kdv_5fsplit_7',['kdv_split',['../namespacemod__kdv.html#ad8ce6e5248e6db5c86d1e4e8b1635f4e',1,'mod_kdv']]],
-  ['kmaxx_8',['kmaxx',['../namespacemod__odeint.html#a4dd539fe2bf3bc713ff7b1275efecbd5',1,'mod_odeint']]],
-  ['kr_9',['kr',['../namespacemod__global__parameters.html#a31226ca76b94f6c33496105fd25e8fd9',1,'mod_global_parameters']]]
+  ['k2_0',['k2',['../namespacemod__gimli.html#aac5e26b134ad8138022f33623336f425',1,'mod_gimli']]],
+  ['k3_1',['k3',['../namespacemod__gimli.html#a7b979f6925d00c27ecf57992651e47d6',1,'mod_gimli']]],
+  ['kappa_5fvals_2',['kappa_vals',['../namespacemod__opal__opacity.html#a7dd97579ace2715cddebb4296ab3101f',1,'mod_opal_opacity']]],
+  ['kappae_5fvals_3',['kappae_vals',['../namespacemod__cak__opacity.html#adbf22c7880102b71259e10a7bbda2479',1,'mod_cak_opacity']]],
+  ['kb_5fcgs_4',['kb_cgs',['../namespacemod__constants.html#a33660422af2eb7bceb7c9162c04c0688',1,'mod_constants']]],
+  ['kb_5fsi_5',['kb_si',['../namespacemod__constants.html#a9cf643c04abeb836326c027924b53d7c',1,'mod_constants']]],
+  ['kdv_5fdelta_6',['kdv_delta',['../namespacemod__kdv.html#abc5190eca5496e7f28668f0d9d92251f',1,'mod_kdv']]],
+  ['kdv_5forder_7',['kdv_order',['../namespacemod__kdv.html#a6d24bd8450000c731e6505d07e5046de',1,'mod_kdv']]],
+  ['kdv_5fsource_5fterm_8',['kdv_source_term',['../namespacemod__nonlinear__phys.html#a673d1030c11f7d04b8102184cde63dd6',1,'mod_nonlinear_phys']]],
+  ['kdv_5fsplit_9',['kdv_split',['../namespacemod__kdv.html#ad8ce6e5248e6db5c86d1e4e8b1635f4e',1,'mod_kdv']]],
+  ['kmaxx_10',['kmaxx',['../namespacemod__odeint.html#a4dd539fe2bf3bc713ff7b1275efecbd5',1,'mod_odeint']]],
+  ['kr_11',['kr',['../namespacemod__global__parameters.html#a31226ca76b94f6c33496105fd25e8fd9',1,'mod_global_parameters']]]
 ];
